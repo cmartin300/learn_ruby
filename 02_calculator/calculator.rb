e51b1d3f22e15835bbdd15dc683445337917c57e
Lobstaa <martin.christophercharles@gmail.com>
2020-03-07 15:34:03 -0700

    01 && 02 completed

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,34 @@
 #write your code here
+
+
+def add (a, b)
+    a + b
+end
+
+def subtract (a, b)
+    a - b
+end
+
+def sum (array)
+    array.inject(0, :+)
+end
+
+def multiply (array)
+    array.inject(:*)
+end
+
+def power (a, b)
+    a ** b
+end
+
+def factorial (int)
+    i = int
+    result = 1
+
+    while (i > 0)
+        result *= i
+
+        i -= 1
+    end
+    result
+end
